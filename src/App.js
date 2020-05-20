4f098bf259172ef38d1bb0d2200ef8652d53f23a
koushith <koushith97@gmail.com>
2020-05-21 01:00:05 +0530

    firebase auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.css';
+import 'bootsrtap/dist/css/bootstrap.min.css';
+
+// react router
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+// toast
+import 'react-toastify/dist/ReactTostify.min.css';
+
+// firebase
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// Components AKA Pages
 
 function App() {
   return (
