7d32dc305e0a88997258cc2ad640d5dd4a5a1cba
koushith <koushith97@gmail.com>
2020-05-21 00:40:24 +0530

    Added React Version

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
