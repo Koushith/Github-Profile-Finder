541ea1620978d36cd23c78dc8e7cb90d01c45e2f
koushith <koushith97@gmail.com>
2020-05-21 23:37:56 +0530

    updated

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -15,6 +15,15 @@ import { Link } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 
 const Header = () => {
+  // values avaliable in the userconyext is now avaliable inside context variable
+  const context = useContext(UserContext);
+  // state for toggle bar menu
+  const [isOpen, setIsOpen] = useState(false);
+  // true-false flipping switches
+  const toggle = () => {
+    return setIsOpen(!isOpen);
+  };
+
   return (
     <Navbar color='info' light expand='md'>
       <NavbarBrand>
@@ -22,17 +31,24 @@ const Header = () => {
           GitHub Profile Fetcher
         </Link>
       </NavbarBrand>
-      <NavbarToggler />
-      <Collapse navbar>
+      {/* onclick and collapse- bootstrap docs */}
+      <NavbarToggler onClick={toggle} />
+      <Collapse navbar isOpen={isOpen}>
         <Nav className='ml-auto' navbar>
           <NavItem>
-            <NavLink className='text-white'>Signup</NavLink>
+            <NavLink className='text-white' tag={Link} to='/signup'>
+              Signup
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink className='text-white'>Signin</NavLink>
+            <NavLink className='text-white' tag={Link} to='/signin'>
+              Signin
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink className='text-white'>Logout</NavLink>
+            <NavLink className='text-white' tag={Link} to='/'>
+              Logout
+            </NavLink>
           </NavItem>
         </Nav>
       </Collapse>
